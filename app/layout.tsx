0cc52973d326535385925037b3cf5df21dd4d4ce
Aahad Siddiqui <aahadsiddiqui@Mac.local.tld>
2025-12-01 22:12:48 -0500

    Add favicon with Daniel Clarke logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         <main className="min-h-screen">{children}</main>
